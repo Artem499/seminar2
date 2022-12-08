a7851ef398e1da36647622bb3f85d8946b5d71c0
Artem <1992.s.a.a@gmail.com>
2022-12-08 15:17:22 +0300

    otput result

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Enter a number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 100;
+int result1 = result /10;
+if(number < 100 || number > 999)
+{
+    Console.WriteLine("Error! Enter three-digit number!");
+}
+else
+{
+    Console.WriteLine("result: " +result1);
+}
+
+
+
